374865d4e8253577a3f4bab5d1d8b03f200f82fa
Ivan Djikovski <ivan.djikovski@wsa.com>
2022-10-14 08:13:33 +0200

    made changes

--- AppiumTestSirona/AppiumTestSirona/BaseTest.cs
+++ AppiumTestSirona/AppiumTestSirona/BaseTest.cs
@@ -17,10 +17,10 @@ namespace AppiumTestSirona
 
             var driverOption = new AppiumOptions();
             driverOption.AddAdditionalCapability(MobileCapabilityType.PlatformName,"Android");
-            driverOption.AddAdditionalCapability(MobileCapabilityType.PlatformVersion,"12");
-            driverOption.AddAdditionalCapability(MobileCapabilityType.DeviceName, "emulator-5554");
-            driverOption.AddAdditionalCapability("appPackage", "com.companyname.realestateapp");
-            driverOption.AddAdditionalCapability("appActivity", "crc6487c6afeb28c0d00b.MainActivity");
+            driverOption.AddAdditionalCapability(MobileCapabilityType.PlatformVersion,"11");
+            driverOption.AddAdditionalCapability(MobileCapabilityType.DeviceName, "emulator-5556");
+            driverOption.AddAdditionalCapability("appPackage", "com.test.apiumtestapp");
+            driverOption.AddAdditionalCapability("appActivity", "crc64f54a05b22e7375c7.MainActivity");
 
             _driver = new AndroidDriver<AndroidElement>(new Uri("http://localhost:4723/wd/hub"), driverOption);
             _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(4);
@@ -31,13 +31,14 @@ namespace AppiumTestSirona
         [Test]
         public void InitTest()
         {
-            //var a = _driver.FindElementByAccessibilityId("test");
+            var a = _driver.FindElementByAccessibilityId("TestAutomation");
+            var c = a.Text;
             //var b = _driver.FindElementById("test");
             //var c = _driver.FindElementByClassName("test");
             //var d = _driver.FindElementByCssSelector("test");
             //var e = _driver.FindElementByName("test");
 
-            var a = _driver.PageSource;
+            var b = _driver.PageSource;
         }
 
         [TearDown]
